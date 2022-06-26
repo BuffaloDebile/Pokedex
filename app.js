4545ea28879feff7a876dabaed52ef94a3d03e10
Paul-Adrien <pauladrienbourgue@gmail.com>
2022-06-26 18:42:35 +0200

    Manage API data to get them in French

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
-
+let allPokemon = [];
+let TableauFin = [];
 const searchInput = document.querySelector('.recherche-poke input');
 
 // Appel API pokedex
@@ -8,10 +9,43 @@ function fetchPokemonBase() {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
         .then(reponse => reponse.json())
         .then((allPoke) => {
-            console.log(allPoke.results[0].name);
+            
+            allPoke.results.forEach((pokemon) => {
+                fetchPokemonComplet(pokemon);
+            })
+
         });
 }
 fetchPokemonBase();
+
+function fetchPokemonComplet(pokemon) {
+    let objPokemonFull = {};
+    let url = pokemon.url;
+    let nameP = pokemon.name;
+
+    fetch(url)
+    .then(reponse => reponse.json())
+    .then((pokeData) => {
+        objPokemonFull.pic = pokeData.sprites.front_default;
+        objPokemonFull.type = pokeData.types[0].type.name;
+
+        fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
+        .then(reponse => reponse.json())
+        .then((pokeData) => {
+
+            
+            objPokemonFull.name = pokeData.names[4].name;
+            allPokemon.push(objPokemonFull);
+          
+
+          if (allPokemon.length === 151) {
+            console.log(allPokemon);
+            
+          }
+
+        })
+    })
+}
 
 
 
